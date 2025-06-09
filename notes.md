4ed4fa2dd32b5d255b75da6bae7b607c6707c17c
Chris Fagan <cfagan88@hotmail.co.uk>
2025-06-09 12:38:11 +0100

    Update to styling for desktop view

--- notes.md
+++ notes.md
@@ -1,2 +1,4 @@
 Reusability - classNames generic enough to be used across products/categories
  - if no previous price, layout should not be impacted
+
+ Used 750px as boundary for mobile to desktop, but is there a standard/convention?
--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,35 @@
-import "./index.css"
+import "./index.css";
 
 function App() {
   return (
     <div>
       <div className="item-card">
-        <img className="item-image" src="src/assets/image-product-mobile.jpg" alt="A bottle of CHANEL Paris Gabrielle Essence Eau De Parfum"/>
+        <picture className="image-container">
+          <source
+            media="(min-width: 750px)"
+            srcSet="src/assets/image-product-desktop.jpg"
+          />
+          <img
+            className="item-image"
+            src="src/assets/image-product-mobile.jpg"
+            alt="A bottle of CHANEL Paris Gabrielle Essence Eau De Parfum"
+          />
+        </picture>
         <div className="item-info">
-        <p className="item-category">Perfume</p>
-        <h2>Gabrielle Essence Eau De Parfum</h2>
-        <p className="item-desc">
-          A floral, solar and voluptuous interpretation composed by Olivier
-          Polge, Perfumer-Creator for the House of CHANEL.
-        </p>
-        <div className="prices">
-        <p className="curr-price">$149.99</p>
-        <p className="prev-price">$169.99</p>
-        </div>
-        <button className="add-cart-btn">
-          <img src="src/assets/icon-cart.svg"/>
-          Add to Cart</button>
+          <p className="item-category">Perfume</p>
+          <h2>Gabrielle Essence Eau De Parfum</h2>
+          <p className="item-desc">
+            A floral, solar and voluptuous interpretation composed by Olivier
+            Polge, Perfumer-Creator for the House of CHANEL.
+          </p>
+          <div className="prices">
+            <p className="curr-price">$149.99</p>
+            <p className="prev-price">$169.99</p>
+          </div>
+          <button className="add-cart-btn">
+            <img src="src/assets/icon-cart.svg" alt="Shopping cart icon" />
+            Add to Cart
+          </button>
         </div>
       </div>
       {/* <div className="attribution">
